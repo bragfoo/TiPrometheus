82530f8449585e1f78261df2159e32c9e89030cd
teamNoble <kevin1993@qq.com>
2018-12-01 22:20:20 +0800

    Revert "fix"
    This reverts commit f298997

--- src/modules/adapter/writer.go
+++ src/modules/adapter/writer.go
@@ -21,6 +21,8 @@ var indexLabelMap sync.Map
 var oriMsgMap sync.Map
 
 func RemoteWriter(data prompb.WriteRequest) {
+	log.Println("tttttttttt")
+	log.Println(data.Timeseries)
 	for _, oneDoc := range data.Timeseries {
 		labels := oneDoc.Labels
 		samples := oneDoc.Samples
@@ -28,15 +30,15 @@ func RemoteWriter(data prompb.WriteRequest) {
 
 		//build index and return labelMD
 		labelMD := buildIndex(labels, samples)
-		//log.Println(labelMD)
 
 		// write
 		writeTimeseriesData(labelMD, samples)
 		//log.Println(metaLabelMap)
 		labelsByte := lib.GetBytes(labels)
 		SaveOriDoc(labelMD, labelsByte)
-		//time.Sleep(10 * time.Second)
 	}
+	//time.Sleep(10*time.Minute)
+	time.Sleep(10*time.Second)
 }
 
 func MakeNewMD(initByte []byte) string {
@@ -69,36 +71,57 @@ func buildIndex(labels []*prompb.Label, samples []*prompb.Sample) string {
 
 		key := buffer.String()
 		//log.Println("Write label md:", key, labelMD)
-
 		indexStatus := "index:status:" + v.Name + "#" + v.Value + "+" + labelMD
 		indexStatusKey, gErr := tikv.Get([]byte(indexStatus))
 		log.Println("indexStatus:", indexStatusKey, gErr)
 
 		if "" == indexStatusKey.Value {
 			tikv.Puts([]byte(indexStatus), []byte("1"))
-
-			//wtire tikv
-			oldKey, err := tikv.Get([]byte(key))
-			log.Println("oldKey", oldKey, err)
-
-			if oldKey.Value == "" {
-				//fitrst
-				tikv.Puts([]byte(key), []byte(labelMD))
-			} else {
-
-				b := bytes.NewBufferString(oldKey.Value)
-				b.WriteString(",")
-				b.WriteString(labelMD)
-				v := b.Bytes()
-				metaLabelMap.Store(key, string(v))
-				tikv.Puts([]byte(key), v)
-			}
+			tikv.Puts([]byte(key), []byte(labelMD))
 		} else {
 
-			indexStatusKey, gErr := tikv.Get([]byte(indexStatus))
-			log.Println("indexStatus:", indexStatusKey, gErr)
+			indexData, gErr := tikv.Get([]byte(key))
+			log.Println("indexData:", indexData.Key, indexData.Value, gErr)
+			//if "" == indexData.Value {
+			//	tikv.Puts([]byte(key), []byte(labelMD))
+			//} else {
+
+			//tikv.Puts([]byte(indexStatus), []byte("1"))
+			oldKey, err := tikv.Get([]byte(key))
+			log.Println("oldKey", oldKey, err)
+			b := bytes.NewBufferString(oldKey.Value)
+			b.WriteString(",")
+			b.WriteString(labelMD)
+			v := b.Bytes()
+			metaLabelMap.Store(key, string(v))
+			tikv.Puts([]byte(key), v)
+			//}
 
 		}
+
+		//tikv.Puts([]byte(key), []byte(labelMD))
+
+		//if actual, loaded := metaLabelMap.LoadOrStore(key, labelMD); loaded {
+		//	//insert value into old map value
+		//	b := bytes.NewBufferString(actual.(string))
+		//	b.WriteString(",")
+		//	b.WriteString(labelMD)
+		//	v := b.Bytes()
+		//	metaLabelMap.Store(key, string(v))
+		//	tikv.Puts([]byte(key), v)
+		//} else {
+		//	//重启 如果map里没有 需要直接写tikv
+		//	//载入tikv中已有的信息
+		//	if kv, err := tikv.Get([]byte(key)); "" != kv.Value && nil == err {
+		//		b := bytes.NewBufferString(kv.Key)
+		//		b.WriteString(",")
+		//		b.WriteString(labelMD)
+		//		v := b.Bytes()
+		//		metaLabelMap.Store(key, string(v))
+		//		tikv.Puts([]byte(key), v)
+		//	}
+		//	tikv.Puts([]byte(key), []byte(labelMD))
+		//}
 
 	}
 
@@ -115,54 +138,25 @@ func buildIndex(labels []*prompb.Label, samples []*prompb.Sample) string {
 	for _, v := range samples {
 		tikv.Puts(timeIndexBytes, int64ToBytes(v.Timestamp))
 		//log.Println("Write time index:", string(timeIndexBytes), string(int64ToBytes(v.Timestamp)))
-	}
 
-	for _, v := range samples {
-
-		indexStatus := "index:status:" + strconv.FormatInt(v.Timestamp, 10) + "#" + strconv.FormatFloat(v.Value, 'E', -1, 64) + "+" + labelMD
-		indexStatusKey, gErr := tikv.Get([]byte(indexStatus))
-		log.Println("indexStatus:", indexStatusKey, gErr)
-
-		if "" == indexStatusKey.Value {
-			tikv.Puts([]byte(indexStatus), []byte("1"))
-
-			//wtire tikv
-			oldKey, err := tikv.Get(timeIndexBytes)
-			log.Println("oldKey", oldKey, err)
-
-			if oldKey.Value == "" {
-				//fitrst
-				tikv.Puts(timeIndexBytes, int64ToBytes(v.Timestamp))
-			} else {
-
-				b := bytes.NewBufferString(oldKey.Value)
-				b.WriteString(",")
-				b.Write(int64ToBytes(v.Timestamp))
-				v := b.Bytes()
-				metaLabelMap.Store(timeIndexBytes, string(v))
-				tikv.Puts(timeIndexBytes, v)
-			}
-		} else {
-
-			indexStatusKey, gErr := tikv.Get([]byte(indexStatus))
-			log.Println("indexStatus:", indexStatusKey, gErr)
-
-		}
 	}
 
 	return labelMD
 }
 
 func writeTimeseriesData(labelMD string, samples []*prompb.Sample) {
+
 	for _, v := range samples {
 		buffer := bytes.NewBufferString("timeseries:doc:")
 		buffer.WriteString(labelMD)
 		buffer.WriteString(":")
 		buffer.WriteString(strconv.FormatInt(v.Timestamp, 10))
 		key := buffer.Bytes()
+
 		//	//write to tikv
 		tikv.Puts(key, []byte(strconv.FormatFloat(v.Value, 'E', -1, 64)))
 		//log.Println("Write timeseries:", string(key), strconv.FormatFloat(v.Value, 'E', -1, 64))
+
 	}
 }
 
@@ -172,6 +166,7 @@ func SaveOriDoc(labelMD string, originalMsg []byte) {
 	key := buffer.Bytes()
 	tikv.Puts(key, originalMsg)
 	//log.Println("Write meta:", string(key), string(originalMsg))
+
 }
 
 func int64ToBytes(i int64) []byte {
